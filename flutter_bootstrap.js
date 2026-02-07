891310a858676ad7e41ad183c361d3388b9a4481
take-ae86 <genspark_flutter_dev@genspark.ai>
2026-02-07 15:32:16 +0000

    法則性画像修正v3 - 閾値200に強化(薄グレー完全除去)＋中央揃え再適用

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2934063417"
+    serviceWorkerVersion: "3325473187"
   }
 });
