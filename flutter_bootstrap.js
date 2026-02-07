be2c39e36d13a664ac3aad1d1a9b707b1be25573
take-ae86 <genspark_flutter_dev@genspark.ai>
2026-02-07 13:37:25 +0000

    ビルド成果物をルートに反映 - アイコン・ロゴ差し替え完了

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3120176948"
+    serviceWorkerVersion: "2184936017"
   }
 });
